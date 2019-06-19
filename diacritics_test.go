6c010f1e97198200162e96e655fd15da0c76ac3d
Enrico Hoffmann <dasrick@gmail.com>
2019-06-19 15:10:11 +0200

    add implementation (THXstefanberkner)

--- diacritics_test.go
+++ diacritics_test.go
@@ -1,7 +1,29 @@
-package main
+package godiacritics
 
-import "testing"
+import (
+	"testing"
 
-func TestFooBar(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
 
+func TestRemoval(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected string
+	}{
+		{input: "a", expected: "a"},
+		{input: "ä", expected: "a"},
+		{input: "Ä", expected: "A"},
+		{input: "ß", expected: "ss"},
+		{input: "Č", expected: "C"},
+		{input: "Ł", expected: "L"},
+		{input: "Ä möre ȼꝋmpleᶍ ⱸxamplé", expected: "A more complex example"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.input, func(t *testing.T) {
+			actual := Normalize(c.input)
+			assert.Equal(t, c.expected, actual)
+		})
+	}
 }
